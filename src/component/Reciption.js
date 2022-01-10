b7ad7f32191ba2c17a1319b0ee29912eb527ab8d
mohammedyosef761 <mohammedyosef712@gmail.com>
2022-01-10 23:40:41 +0200

    animation_react

--- src/component/Reciption.js
+++ src/component/Reciption.js
@@ -5,8 +5,11 @@ const reciption = () => {
     return (
         <div className='container'>
         <div className='img'>
-        <h3 className='text'>
-         <span>shopping now</span></h3>
+        <div className='animated-text'>
+        <h3 className='text'><span>happy shopping</span></h3>
+        <h3 className='text'><span>Sale 50%</span></h3>
+        <h3 className='text'><span>shopping now</span></h3>
+         </div>
         </div>
         </div>
     )
